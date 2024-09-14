b3f66eef168728520de05bce77f9d4c94acbf41d
TAMILKUMARAN S <100863096+TAMILKUMARAN4112002@users.noreply.github.com>
2024-09-14 12:42:20 +0530

    Create minWindowSubstring.java

---
+++ minWindowSubstring.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String [] args){
+         Scanner sc= new Scanner(System.in);
+         String str = sc.nextLine();
+         String str1 = sc.nextLine();
+         int count = str1.length();
+         boolean tag=false;
+         for(int size=count;size<=str.length();size++){
+             for(int i=0;i<=str.length()-size;i++){
+                 String t = "";
+                 int count1 = 0;
+                 for(int j=i;j<i+size;j++){
+                     t = t + str.charAt(j);
+                 }
+                 for(int h=0;h<t.length();h++){
+                     for(int q=0;q<str1.length();q++){
+                         if(t.charAt(h)==str1.charAt(q)){
+                             count1++;
+                         }
+                     }
+                 }
+                 if(count==count1){
+                     System.out.print(t);
+                     tag=true;
+                     return;
+                 }
+             }
+         }
+         if(tag){
+             System.out.print("No Substring");
+         }
+
+    }
+}
